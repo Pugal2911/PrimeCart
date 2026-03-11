f6e05716a448c3cc2f63f3b1556659e2a3df3e9e
kenstacktechnologies <sripugaldevaraj@gmail.com>
2026-03-11 17:00:57 +0530

    Initial commit - PrimeCart project

--- app.py
+++ app.py
@@ -1408,5 +1408,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5050, debug=True)
-
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
